84e2b38f81299ce9ac65239f97fb56b161f51b0e
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-11-13 22:30:38 +0800

    > make 181250117 root Linux debian 4.19.0-12-amd64 #1 SMP Debian 4.19.152-1 (2020-10-18) x86_64 GNU/Linux  22:30:38 up  1:19,  1 user,  load average: 0.18, 0.14, 0.16 701a037c1990b45b90b8c753e93a78cc3946af31

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,11 @@
 
 extern FILE* yyin;
 extern "C" int yylex();
+extern "C" void yyrestart(FILE*);
 
 int error_flag;
 int second_flag;
-extern int yylineno;
+//extern int yylineno;
 
 int main(int argc, char** argv){
     error_flag = 0;
@@ -20,10 +21,11 @@ int main(int argc, char** argv){
     while (yylex()!=0);
     // second time
     if(error_flag==0){
+        yyrestart(yyin);
         second_flag = 1;
-        yylineno = 1;
-        yyin = fopen(argv[1],"r");
-            while (yylex()!=0);
+        //yylineno = 1;
+        //yyin = fopen(argv[1],"r");
+        while (yylex()!=0);
     }
 
     return 0;
