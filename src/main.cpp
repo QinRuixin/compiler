f7b9a508586d5badeaf8df72417d7194c0b58b5a
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-12-15 09:10:45 +0800

    > make 181250117 root Linux debian 4.19.0-12-amd64 #1 SMP Debian 4.19.152-1 (2020-10-18) x86_64 GNU/Linux  09:10:45 up 24 min,  1 user,  load average: 0.15, 0.39, 0.41 6fa6859e45596008e3cbed4b22a74d3c6e9c163

--- src/main.cpp
+++ src/main.cpp
@@ -140,11 +140,11 @@ void AnalasysForSpecifier(tree_node* ptr){
         }else{//  if(strcmp(ptr->node_name,"float")==0)
             global_type_ptr->u.basic = 1;
         }
-        AnalasysForVarDec(ptr->child_node[0]);
+        //AnalasysForVarDec(ptr->child_node[0]);
     }else{//  if(ptr->child_node[0]->node_type  == ENUM_StructSpecifier)
         global_type_ptr->kind = global_type_ptr->STRUCTURE;
         //todo
-        AnalasysForStructSpecifier(ptr->child_node[0]);
+        AnalasysForStructSpecifier(ptr->child_node[1]);
     }
     
 
@@ -208,7 +208,8 @@ void AnalasysForVarDec(tree_node* ptr){
         Sysmtable.insert(std::pair<std::string,Sysmtable_item>(name,cur_item));
     }
     std::cout << ptr->node_name << std::endl;
-
+    std::cout << ptr->node_type << std::endl;
+    
 }
 
 //todo
