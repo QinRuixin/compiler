78c9aa0c7fad91763f81c3fc15ea73cf0ebaad60
tracer-compiler2020 <compilers@software.nju.edu.cb>
2021-01-20 23:50:06 +0800

    > make 181250117 root Linux debian 4.19.0-12-amd64 #1 SMP Debian 4.19.152-1 (2020-10-18) x86_64 GNU/Linux  23:50:06 up  9:17,  1 user,  load average: 0.20, 0.45, 0.48 6b8009648dec6d549e936dff6f56c45a3e650629

--- src/ir.cpp
+++ src/ir.cpp
@@ -72,6 +72,7 @@ void Translate(tree_node* ptr,std::map<std::string, struct Sysmtable_item>& Sysm
     switch (cur_type)
     {
     case ENUM_Exp:
+        cout <<"ENUM_Exp : " << endl;
         TranslateExp(ptr, Sysmtable, nullptr);
         break;
     //todo
