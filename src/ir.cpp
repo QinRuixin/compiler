e5bba26a96908b0abeae3d551fe089e81af9654e
tracer-compiler2020 <compilers@software.nju.edu.cb>
2021-01-21 07:41:05 +0800

    > make 181250117 root Linux debian 4.19.0-12-amd64 #1 SMP Debian 4.19.152-1 (2020-10-18) x86_64 GNU/Linux  07:41:05 up 17:08,  1 user,  load average: 0.76, 0.60, 0.48 1e123c7146c55775145eb39ba516ae77489ef61b

--- src/ir.cpp
+++ src/ir.cpp
@@ -323,9 +323,9 @@ cout << "ok " << ptr->child_num <<endl;
             cur_code_goto_lb3->kind = cur_code_goto_lb3->GOTO;
             append_code(cur_code_goto_lb3);
             append_code(cur_code_lb2);
-cout << ptr->child_node[6]->child_num << endl;
+//cout << ptr->child_node[6]->child_num << endl;
             TranslateStmt(ptr->child_node[6], Sysmtable); // cur_code3
-            InterCode* cur_code_lb3 = new_sinop_code(operand_lb1);
+            InterCode* cur_code_lb3 = new_sinop_code(operand_lb3);
             cur_code_lb3->kind = cur_code_lb3->LABEL;
             append_code(cur_code_lb3);
         }else{
