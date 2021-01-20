1c5bfd151a1c58a333205ef5b08c6f077949d8c0
tracer-compiler2020 <compilers@software.nju.edu.cb>
2021-01-21 00:00:27 +0800

    > make 181250117 root Linux debian 4.19.0-12-amd64 #1 SMP Debian 4.19.152-1 (2020-10-18) x86_64 GNU/Linux  00:00:27 up  9:27,  1 user,  load average: 0.82, 0.47, 0.47 39fe1807e622a46cfc73c3da3a4fcdad7b33cafd

--- src/ir.cpp
+++ src/ir.cpp
@@ -143,6 +143,7 @@ void TranslateExp(tree_node* ptr,std::map<std::string, struct Sysmtable_item>& S
     }
     //Exp1 ASSIGNOP Exp2
     if(ptr->child_node[1]->node_type== ENUM_ASSIGNOP){
+cout <<  "ENUM_ASSIGNOP" <<endl;       
         auto it = Sysmtable.find(ptr->node_name); // Exp1 -> ID
         string t1 = new_temp();
         Operand* operand_t1 = new_var_operand(t1);
